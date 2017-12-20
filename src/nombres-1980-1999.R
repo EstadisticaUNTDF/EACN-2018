0e128b6ff56b3649902ac0852317fab056d678be
Luciano Selzer <luciano.selzer@gmail.com>
2017-12-20 10:45:26 -0300

    arreglado la repeticion de nombres dentro de un mismo año

--- src/nombres-1980-1999.R
+++ src/nombres-1980-1999.R
@@ -1,11 +1,17 @@
 library(readr)
 library(tidyr)
 library(dplyr)
+library(magrittr)
 
 archivos <- dir("Labs/data/", pattern = "[0-9]{4}.csv", full.names = TRUE)
 
 nombres <- lapply(archivos, read_csv) %>% 
   do.call(bind_rows, .)
+
+nombres %<>% group_by(nombre, anio) %>% 
+  summarise(cantidad = sum(cantidad)) %>% 
+  ungroup()
+
 
 saveRDS(nombres, "Labs/data/nombres-1980-1999.RDS")
 
